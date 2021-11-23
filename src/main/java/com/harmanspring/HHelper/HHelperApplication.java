d776170b51e530bd713bf1b9cd805313291adcfa
anof1r <Deniska20121261999>
2021-11-24 00:21:25 +0300

    JavaSpring backend first commit

---
+++ src/main/java/com/harmanspring/HHelper/HHelperApplication.java
@@ -0,0 +1,13 @@
+package com.harmanspring.HHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harmanspring/HHelper/HHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmanspring.HHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
